317b7617ac2a88dfd28cb111741b096d13fdac46
Luishi1 <luis.oviedo.et26@gmail.com>
2026-03-24 16:09:20 -0300

    Initial commit: Estructura base de Laravel para La Página de Física

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+import './bootstrap';
+import Alpine  from 'alpinejs';
+//import { StaticCanvas, FabricText } from 'fabric';
+import { fabric } from 'fabric';
+
+window.Alpine = Alpine;
+Window.fabric = fabric; 
+
+Alpine.start();
